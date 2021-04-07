b6eef2b130a5493faf5e53e6d76b351a57deea87
Shreya0904 <70674052+Shreya0904@users.noreply.github.com>
2021-04-06 20:58:47 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,105 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var bird, elephant, doggy, oneeye, robot, tiger;
+
+var ocean, ocean2, startingGround, startScreen, 
+skip, quit, challangeScreen, background, 
+leaderBoard, help, heart,characterScreen, gameOver;
+
+var blocks=[];
+var score=0;
+
+function preload() {
+ocean_png=loadImage("images/ocean.png")
+startScreen_png=loadImage("images/startScreen.png")
+skip_png=loadImage("images/skip.png")
+quit_png=loadImage("images/quit.png")
+challengeScreen_png=loadImage("images/challengeScreen.png")
+Background_png=loadImage("images/Background.png")
+leaderBoard_png=loadImage("images/leaderBoard.png")
+help_png=loadImage("images/help.png")
+heart_png=loadImage("images/heart.png")
+blocks_png=loadImage("images/blocks.png")
+characterScreen_png=loadImage ("images/characterScreen.png")
+gameOver_png=loadImage("images/gameOver.png")
+
+bird_png=loadImage("characters/bird.png")
+elephant_png=loadImage("characters/elephant.png")
+doggy_png=loadImage("characters/doggy.png")
+oneeye_png=loadImage("characters/oneeye.png")
+robot_png=loadImage("characters/robot.png")
+tiger_png=loadImage("characters/tiger.png")
+
+sounds_gameOverSound=loadSound("Sounds/sounds_gameOverSounds.ogg")
+sounds_getReady=loadSound("Sounds/sounds_getReady.ogg")
+sounds_jumping_sound_1=loadSound("Sounds/sounds_jumping_sound_1.ogg")
+}
+
+
+function setup() {
+  createCanvas(800,400);
+
+  engine = Engine.create();
+  world = engine.world;
+
+var ocean=createSprite(200,250,10,10);
+ocean.addImage(ocean_png);
+ocean.scale=1.5
+
+var ocean2=createSprite(600,250,10,10);
+ocean2.addImage(ocean_png);
+ocean2.scale=1.5
+
+  startingGround=new Ground(403,273,400,40);
+  player=new Bird(400, 200, 50)
+console.log(startingGround);
+console.log(player.body.position.y)
+
+
+//block=new Blocks(270,231,100,30);
+}
+
+function draw() {
+  background(Background_png);  
+  Engine.update(engine);
+   
+  if(frameCount%100===0){
+    blocks.push(new Blocks(random(270,370),231,100,30));
+   // Matter.Body.setVelocity(blocks.body, {x:0, y:8});
+  }
+  if(player.body.position.x>600||player.body.position.x<200){
+    text("GAME OVER,",382,167);
+  }
+
+  for (var b = 0; b < blocks.length; b++) {
+    blocks[b].display();
+    }
+
+startingGround.display();
+player.display();
+
+  drawSprites();
+  text(mouseX+","+mouseY,mouseX,mouseY);
+  textSize(30);
+  text("Score " +score,53,32)
+}
+
+function keyPressed(){
+  if(keyCode === 32){
+  Matter.Body.setVelocity(player.body, {x:0, y:8}); 
+  sounds_jumping_sound_1.play();
+  score=score+50;
+  }
+  }
